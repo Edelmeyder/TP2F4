0832c249a119ea7cab57f2b66feef6e68f3b218a
joaquinmir <joaquinmir99@gmail.com>
2022-11-05 21:35:30 -0300

    Now the server stores the wifi data

--- database/main.js
+++ database/main.js
@@ -1,9 +1,10 @@
 const http = require('http');
 
-/*const MongoClient = require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient
 const url = 'mongodb://127.0.0.1:27017'
 let db;
-let dataCollection;
+let commandsCollection;
+let wifiCollection;
 const options = { ordered: true };
 MongoClient.connect(
   url,
@@ -16,25 +17,17 @@ MongoClient.connect(
       return console.log(err)
     }
     db = client.db('autitoDatabase')
-    // Specify the database you want to access
-    dataCollection = db.collection('commands')
+  
+    commandsCollection = db.collection('commands')
+    wifiCollection = db.collection('wifis')
     
     console.log(`MongoDB Connected: ${url}`)
   }
 )
 
-async function test (){
-try {
-  const asd = [{'value':'J','date':123},{'value':'P','date':124},{'value':'F','date':125}]
-  console.log(asd)
-  await dataCollection.insertMany(asd , options);
-  }
-  catch(err){
-    console.log(err)
-  }
-}*/
+
 var intervalId = setInterval(request
-  , 3000);
+  , 15000);
   
 async function request(){  
 http.get('http://192.168.4.1/data', res => {
@@ -48,21 +41,42 @@ http.get('http://192.168.4.1/data', res => {
   res.on('end', async () => {
     let dataString = Buffer.concat(data).toString();
     let jsonData = JSON.parse(dataString);
-    /*if(jsonData["commands"]){
+    if(jsonData["commands"]){
       const index = jsonData["index"];
       if(jsonData["commands"][index + 1]){
         let orderedArray = [...jsonData["commands"].slice(index+1)]
         orderedArray = [...orderedArray,...jsonData["commands"].slice(0,index+1)]
         jsonData["commands"] = [...orderedArray]
       }
-      console.log(jsonData["commands"]);
-      console.log(jsonData["wifi"]);
-      
+      try{
+        await commandsCollection.insertMany(jsonData["commands"] , options);
+      }
+      catch(err){
+        console.log(err)
+      }
     }
     else{
       console.log("Empty Data")
-    }*/
-    console.log(jsonData);
+    }
+  
+  if(jsonData["wifi"]){
+    jsonData["wifi"].forEach(async el => {
+      try{
+        await wifiCollection.update(
+          {
+            SSID : el.SSID
+          }, 
+           {
+            $setOnInsert: el
+           },
+           {upsert: true}
+        )
+      }
+      catch(err){
+        console.log(err)
+      }
+    })
+  }
   });
 }).on('error', err => {
   console.log('Error: ', err.message);
