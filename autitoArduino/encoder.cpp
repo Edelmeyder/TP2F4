f87b2069858ec3c52600f7cdc1372f05ec456fea
edelmeyder <edelmiro.7.8.9.5@gmail.com>
2022-11-06 20:01:11 -0300

    Encoder functions with interrupts

--- autitoArduino/encoder.cpp
+++ autitoArduino/encoder.cpp
@@ -1,48 +1,35 @@
 #include "encoder.h"
-#include "motor.h"
-#include "sensor.h"
 #include <Arduino.h>
 
-int pulses;
-
-unsigned long timetick;
+volatile static int pulses;
+volatile static int turns = 0;
+volatile static int turnStart;
+volatile static int turn = 1;
 
 int ENCODER_read(void);
+void encoderIntHandler();
 
 void ENCODER_init() {
   pinMode(ENCODER_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_PIN), encoderIntHandler, RISING);
 }
 
 void ENCODER_start() {
-  pulses = 0;
+  turnStart = pulses;
+  turn = 0;
 }
 
 void ENCODER_wait() {
-  timetick = millis(); // Used to prevent infinite loop in case no pulses are read
-  while (pulses < ENCODER_HOLES && (millis() - timetick) < 500) {
-    if(SENSOR_Verif_Colision()){    //pooling sensor, 1 si se detecto objeto
-      if (!MOTOR_GET_STATE()){  //si es 0 es porque va hacia adelante entonces debemos pararlo
-          MOTOR_stop();
-          break;
-      }
-    }
-    if (ENCODER_read()) { // A whole turn has been made
-      pulses++;
-    }
-    delayMicroseconds(10);
-  }
+  while (!turn);
 }
 
-int ENCODER_read() {
-  // Encoder read should be called periodically on a higher frequency than encoder pulses; at least double
-  static int oldValue = 0;
-  static int lastValidValue = 0;
-  byte value;
-  value = digitalRead(ENCODER_PIN);
-  if (value == oldValue & value != lastValidValue) { // Second debounce and multiple detection prevention
-    lastValidValue = value;
-    return value;
+void encoderIntHandler() {
+  pulses++;
+  if (pulses == ENCODER_HOLES) {
+    turns++;
+    pulses = 0;
   }
-  oldValue = value; // First debounce
-  return 0;
+  if (!turn && pulses == turnStart) {
+    turn = 1;
+  } 
 }
--- autitoArduino/encoder.h
+++ autitoArduino/encoder.h
@@ -2,10 +2,11 @@
 #define _ENCODER_H
 
 #define ENCODER_PIN D2     // Encoder pin
-#define ENCODER_HOLES 30  // Number of holes on a single turn
+#define ENCODER_HOLES 20  // Number of holes on a single turn
 
 void ENCODER_init(void);  // Initializes encoder related pins
 void ENCODER_start(void); // Starts counting pulses
-void ENCODER_wait(void);   // When a full turn has been made stops counting and returns 0; otherwise returns 1
+int ENCODER_distance(void); // Return distance since last call
+void ENCODER_wait(void);   // Waits until a full turn has been made since the call
 
 #endif
